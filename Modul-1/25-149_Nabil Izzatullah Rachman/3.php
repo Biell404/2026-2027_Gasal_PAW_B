5ac72eda49b2b59a81cace790e83af18790975e9
Nabil Izzatullah Rachman <n4bilnizam@gmail.com>
2026-09-25 17:34:08 +0700

    tugas modul 1

---
+++ Modul-1/25-149_Nabil Izzatullah Rachman/3.php
@@ -0,0 +1,23 @@
+<1<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet">
+</head>
+<body>
+<!-- ini non-embeded script -->
+<?php echo "hello world"; ?>
+
+<!-- ini embeded script -->
+<?php
+echo "Hallo world";
+echo "Hillo world";
+echo "Hullo world";
+echo "Hello world";
+echo "Hollo world";
+?>
+
+</body>
+</html>
